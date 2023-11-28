b9a125c4b3e5957001590de085997c6f4cad96d2
Sumeed Kanwar <sumeedkanwar@gmail.com>
2023-11-28 12:02:24 +0500

    "linked"

--- Source.cpp
+++ Source.cpp
@@ -1,25 +1,3 @@
-#include "User_Interface.h"
+#include "Header.h"
 
-int main() {
-
-    int choice = 0;
-    cout << "Welcome to the Race Car Game!\n\n";
-    cout << endl << "1. Play in manual mode\n2. Play in automatic mode\n3. Exit\n";
-    cout << "Enter your choice: ";
-    cin >> choice;
-
-    if (choice == 1) {
-        // Play in manual mode
-    }
-    else if (choice == 2) {
-        // Play in automatic mode
-    }
-    else if (choice == 3) {
-        // Exit
-    }
-    else {
-        cout << "Invalid choice. Please try again.\n";
-    }
-
-    return 0;
-}
+using namespace std;
